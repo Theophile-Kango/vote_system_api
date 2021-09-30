b2952dd03e0727e94e6f172967a6c8c362b207d6
Theophile Kango <fadhili.kango@gmail.com>
2021-09-30 04:15:34 +0200

    Ajout des validations au model candidat

--- spec/models/candidat_spec.rb
+++ spec/models/candidat_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Candidat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'db table' do
+    it { is_expected.to have_db_column(:nom).of_type(:string) }
+    it { is_expected.to have_db_column(:post_nom).of_type(:string) }
+    it { is_expected.to have_db_column(:prenom).of_type(:string) }
+    it { is_expected.to have_db_column(:promotion).of_type(:string) }
+    it { is_expected.to have_db_column(:description).of_type(:text) }
+  end 
+
+  describe 'Validation' do
+    it { is_expected.to validate_presence_of :nom }
+    it { is_expected.to validate_presence_of :promotion }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(create(:date_vote)).to be_valid
+    end
+  end
 end
