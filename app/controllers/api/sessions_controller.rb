3733f81237ebcfb8dfad3164be865b298489d369
Theophile Kango <fadhili.kango@gmail.com>
2021-10-08 04:20:54 +0200

    Resolved the bad credential error on login with invalid matricule

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,12 +4,16 @@ class Api::SessionsController < DeviseTokenAuth::SessionsController
         field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
         q_value = get_case_insensitive_field_from_resource_params(field)
         @resource = find_resource(field, q_value)
-        if (@resource.candidate? || @resource.cp?) && params[:source] == 'admin-system'
-            render_create_error_bad_credentials
-        elsif @resource.cp? && params[:source] == 'candidate-system'
-            render_create_error_bad_credentials
+        if params[:resource].present?
+            if (@resource.candidate? || @resource.cp?) && params[:source] == 'admin-system'
+                render_create_error_bad_credentials
+            elsif @resource.cp? && params[:source] == 'candidate-system'
+                render_create_error_bad_credentials
+            else 
+                super
+            end
         else 
-            super
+            render_create_error_bad_credentials
         end
     end
     
