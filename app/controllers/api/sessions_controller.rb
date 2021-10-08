bbd1cf1e3b773817f62b7b52eae257c5a7a254be
Theophile Kango <fadhili.kango@gmail.com>
2021-10-08 04:30:29 +0200

    Fix the resource problem

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::SessionsController < DeviseTokenAuth::SessionsController
         field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
         q_value = get_case_insensitive_field_from_resource_params(field)
         @resource = find_resource(field, q_value)
-        if params[:resource].present?
+        if @resource.present?
             if (@resource.candidate? || @resource.cp?) && params[:source] == 'admin-system'
                 render_create_error_bad_credentials
             elsif @resource.cp? && params[:source] == 'candidate-system'
